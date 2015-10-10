1eff065a09394394c1604bc4960ff476e8fcf3d7
DarwinFox <DarwinFox@users.noreply.github.com>
2015-10-10 13:14:55 +0200

    Create Comparable.java
     Represents an interface to allow for comparison of various objects.

---
+++ Comparable.java
@@ -0,0 +1,9 @@
+//******************************************************************************************
+//Comparable.java		 	Author: Unknown 
+//
+//Represents an interface to allow for comparison of various objects.
+//******************************************************************************************
+
+public interface Comparable{
+	public int compareTo(RockPaperScissorsHand any);
+}
